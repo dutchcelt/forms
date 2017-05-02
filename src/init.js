575f2856a31262b948c126e82a0d713f7184903d
Egor Kloos <ekloos@anwb.nl>
2017-05-02 20:43:42 +0200

    For development just use native modules

--- src/init.js
+++ src/init.js
@@ -1,20 +1,4 @@
-'use strict';
+import addValidation from "./modules/addValidation.js";
 
-// Add SystemJS
-const SystemJS = document.createElement('script');
-SystemJS.async = true;
-SystemJS.src = 'systemjs/system-production.js';
-document.head.appendChild(SystemJS);
-
-function addValidation() {
-    const forms = document.getElementsByClassName('forms');
-    forms.length && System.import('./modules/addValidation.js').then(module => {
-        [...forms].forEach(form => module.default(form));
-    });
-}
-
-SystemJS.onload = addValidation;
-
-SystemJS.onerror = () => {
-    console.error("Can't load SystemJS ");
-};
+const forms = document.getElementsByClassName('forms');
+addValidation(forms[0]);

fd2d5b1a71f3802bee205fc2ae43525400a68af5
dutchcelt <studio@dutchcelt.com>
2017-05-06 12:36:50 +0200

    Added radio test

--- test.js
+++ test.js
@@ -1,50 +1,59 @@
 import test from 'ava';
 
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
-
 const Loader = require('node-es-module-loader');
 const loader = new Loader();
+const getKeyJS = loader.import('./src/modules/helpers/getKey.js');
+const getValueJS = loader.import('./src/modules/helpers/getValue.js');
 
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 const { document } = (new JSDOM(`<!DOCTYPE html>`)).window;
-const form = document.createElement('form');
 
+const form = document.createElement('form');
 form.name = "test";
 document.body.appendChild(form);
 
-function makeElm(DOMSTR){
-	const frag = document.createElement('div');
-	frag.innerHTML = DOMSTR;
-	const elem = frag.firstChild;
-	form.appendChild(elem);
-	return elem;
+function makeElm(domStringArray){
+	const nodelist = [];
+	domStringArray.forEach(str => {
+		const frag = document.createElement('div');
+		frag.innerHTML = str;
+		const elem = frag.firstChild;
+		form.appendChild(elem);
+		nodelist.push(elem);
+	});
+	return nodelist;
 }
-//
-const input = makeElm(`<input type="text" name="name" id="id" value="test"/>`);
 
-// const radios = [
-// 	makeElm('input', 'type:radio','name:radio','id:radio1','value:1'),
-// 	makeElm('input', 'type:radio','name:radio','id:radio2','value:2','checked:checked'),
-// 	makeElm('input', 'type:radio','name:radio','id:radio3','value:3')
-// ];
-const getKeyJS = loader.import('./src/modules/helpers/getKey.js');
-const getValueJS = loader.import('./src/modules/helpers/getValue.js');
-//
-//
-// test('Get radio', async t => {
-// 	const getkey = await getKeyJS;
-// 	t.is(getkey.default(radios[1]), 'radio');
-// });
-// test('Get Radio Value', async t => {
-// 	const getValue = await getValueJS;
-// 	t.is(getValue.default(radios[1]), '2');
-// });
-//
+
+// RADIOS
+const radios = makeElm([
+	`<input type="radio" name="radio" id="radio0" value="0"/>`,
+	`<input type="radio" name="radio" id="radio1" value="1" checked/>`,
+	`<input type="radio" name="radio" id="radio2" value="2"/>`
+]);
+
+test('Get radio', async t => {
+	const getkey = await getKeyJS;
+	t.is(getkey.default(radios[1]), 'radio');
+});
+test('Get Radio Value', async t => {
+	const getValue = await getValueJS;
+	t.is(getValue.default(radios[1]), '1');
+});
+
+
+// TEXT
+
+const input = makeElm([
+	`<input type="text" name="text" id="textid" value="test"/>`
+]);
+
 test('Get Key', async t => {
 	const getkey = await getKeyJS;
-	t.is(getkey.default(input), 'id');
+	t.is(getkey.default(input[0]), 'textid');
 });
 test('Get Value', async t => {
 	const getValue = await getValueJS;
-	t.is(getValue.default(input), 'test');
+	t.is(getValue.default(input[0]), 'test');
 });
